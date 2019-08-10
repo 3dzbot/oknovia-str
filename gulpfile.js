d95d9f8c0b022297487e6afdbada297a47fec511
3dzbot <zoduaks@ukr.net>
2019-08-10 16:29:50 +0300

    add autoprefixer modules

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var browserSync = require('browser-sync').create();
 // var rename = require('gulp-rename');
 // var uglify = require('gulp-uglify');//оптимизатор js
 // var concat = require('gulp-concat');//объединение js
-// const autoprefixer = require('gulp-autoprefixer');
+const autoprefixer = require('gulp-autoprefixer');
 // const cleanCSS = require('gulp-clean-css');
 var babel = require("gulp-babel");
 
@@ -28,19 +28,18 @@ sass.compiler = require('node-sass');
 gulp.task('sass', function(){
     return gulp.src(path.src.style)
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest(path.dist.style))
+        .pipe(gulp.dest('./src/style'))
 });
 
-// function styles(){
-//     return gulp.src('./src/style/css/*.css')
-//         .pipe(concat('style.css'))
-//         .pipe(autoprefixer({
-//             browsers: ['last 5 versions'],
-//             cascade: false
-//         }))
-//         .pipe(cleanCSS({level: 2}))
-//         .pipe(gulp.dest(path.dist.style))
-// }
+function styles(){
+    return gulp.src('./src/style/*.css')
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions'],
+            cascade: false
+        }))
+        // .pipe(cleanCSS({level: 2}))
+        .pipe(gulp.dest(path.dist.style))
+}
 
 function html() {
     return gulp.src(path.src.html)
@@ -63,7 +62,7 @@ function scripts() {
 gulp.task('scripts', scripts);
 gulp.task('html', html);
 gulp.task('server', server);
-// gulp.task('styles', styles);
+gulp.task('styles', styles);
 
 
 function server() {
@@ -75,7 +74,7 @@ function server() {
     gulp.watch("./src/scss/*.scss", gulp.series('sass')).on('change', browserSync.reload);
     gulp.watch("./src/script/**/*.js", gulp.series('scripts')).on('change', browserSync.reload);
     gulp.watch("./src/*.html", gulp.series('html')).on('change', browserSync.reload);
-    // gulp.watch("./src/style/css/style.css", gulp.series('styles')).on('change', browserSync.reload);
+    gulp.watch("./src/style/style.css", gulp.series('styles')).on('change', browserSync.reload);
 };
 
-gulp.task('default', gulp.parallel('sass', scripts, html, server));
+gulp.task('default', gulp.parallel('sass', styles, scripts, html, server));
